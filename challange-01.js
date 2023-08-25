ab7f8858c2dcd899b26d4e129b99b72700087bbf
raihanbadruz <raihanzaman79@gmail.com>
2023-08-25 22:50:49 +0700

    Done

--- challange-01.js
+++ challange-01.js
@@ -1,11 +1,11 @@
-const menuToggle = document.querySelector(".toggle input");
-const nav = document.querySelector("nav ul");
-const body = document.querySelector("body");
+// const menuToggle = document.querySelector(".toggle input");
+// const nav = document.querySelector("nav ul");
+// const body = document.querySelector("body");
 
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-  body.classList.toggle("cantScroll");
-});
+// menuToggle.addEventListener("click", function () {
+//   nav.classList.toggle("slide");
+//   body.classList.toggle("cantScroll");
+// });
 
 // function noScroll() {
 //   window.scrollTo(0, 0);
