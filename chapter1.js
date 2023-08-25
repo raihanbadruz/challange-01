a6cb461eb4a464cf77af4fc67c1485473aab6424
raihanbadruz <raihanzaman79@gmail.com>
2023-08-25 16:49:53 +0700

    All Done -Jarak SideBar

--- chapter1.js
+++ chapter1.js
@@ -1,8 +1,10 @@
 const menuToggle = document.querySelector(".toggle input");
 const nav = document.querySelector("nav ul");
+const body = document.querySelector("body");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
+  body.classList.toggle("cantScroll");
 });
 
 // function noScroll() {
